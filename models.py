fa24da87f1a518ae6a417d256d3e60c52f9ab1cc
Luis Irizarry <lirizarry6178@gmail.com>
2024-10-02 18:32:33 -0400

    Commit start of exercise

---
+++ models.py
@@ -0,0 +1,20 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+def connect_db(app):
+    db.app = app
+    db.init_app(app)
+
+class Pet(db.Model):
+    """Pet model"""
+    __tablename__ = "pets"
+    
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(50), nullable=False)
+    species = db.Column(db.String(50), nullable=False)
+    photo_url = db.Column(db.String(50), nullable=True, default='https://cdn.pixabay.com/photo/2014/03/25/16/26/dog-297102_1280.png')
+    age = db.Column(db.Integer, nullable=True)
+    notes = db.Column(db.Text, nullable=True)
+    available = db.Column(db.Boolean, nullable=False, default=True)
